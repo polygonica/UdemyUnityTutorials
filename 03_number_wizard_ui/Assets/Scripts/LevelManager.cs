3c6ee3b06e14d95abe4664704588684b8f980418
Matthew Arnold <Matthew Arnold>
2016-01-22 18:39:31 -0800

    finished implementing game 03 (number wizard GUI)

---
+++ 03_number_wizard_ui/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+	public void loadLevel(string name) {
+		SceneManager.LoadScene(name);
+	}
+
+	public void quit() {
+		Application.Quit();
+	}
+}
---
+++ 03_number_wizard_ui/Assets/Scripts/NumberWizardScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NumberWizardScript : MonoBehaviour
+{
+	private int currentGuess = 500;
+	private int maxnum = 1000;
+	private int minnum = 1;
+	private int _guessesLeft = 10;
+
+	public Text guessText;
+
+	// Use this for initialization
+	void Start () {
+		nextGuess();
+	}
+
+	int min(int a, int b) {
+		if (a <= b) {
+			return a;
+		} else {
+			return b;
+		}
+	}
+
+	int max(int a, int b) {
+		if (a >= b) {
+			return a;
+		} else {
+			return b;
+		}
+	}
+
+	public void guessHigher() {
+		minnum = min(currentGuess + 1, maxnum); // avoid raising the minnum above maxnum
+		nextGuess();
+	}
+
+	public void guessLower() {
+		maxnum = max(currentGuess - 1, minnum); // avoid lowering the maxnum below minnum
+		nextGuess();
+	}
+
+	private void nextGuess() {
+		if (--_guessesLeft < 0) SceneManager.LoadScene("Win");
+		currentGuess = Random.Range(minnum, maxnum + 1); // min inclusive, max exclusive
+		guessText.text = currentGuess.ToString();
+	}
+}
